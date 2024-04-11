e7cd8f53dfd1796fd90fc8a8030e8b6ca1515933
Ubuntu <ubuntu@ip-172-31-4-125.us-east-2.compute.internal>
2024-04-11 04:04:51 +0000

    add Commandline boilerplate

---
+++ src/main/java/ca/uoft/drsg/bminstrument/CommandLine.java
@@ -0,0 +1,15 @@
+package ca.uoft.drsg.bminstrument;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CommandLine {
+    private static final Logger LOG = LogManager.getLogger(CommandLine.class);
+   
+    public static void main(String[] args){
+
+		System.out.println("Hello, World!");
+	
+	}
+
+}
