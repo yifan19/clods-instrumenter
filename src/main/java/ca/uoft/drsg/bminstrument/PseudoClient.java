2eb79dd8c513f696d37f9760f6c5a15aa8de604a
Ubuntu <ubuntu@ip-172-31-18-101.us-east-2.compute.internal>
2024-12-04 06:15:20 +0000

    add feature to intake a file of all instrumentation plans

--- src/main/java/ca/uoft/drsg/bminstrument/PseudoClient.java
+++ src/main/java/ca/uoft/drsg/bminstrument/PseudoClient.java
@@ -6,6 +6,10 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,15 +17,17 @@ public class PseudoClient extends Thread {
     private String hostname;
     private int portNumber;
     private String[] cmds;
+    private boolean commandFile;
     private static final Logger LOG = LogManager.getLogger(PseudoClient.class);
 
     /* to test the server's response */
     public ArrayList<String> results;
 
-    public PseudoClient(String hostName, int portNumber, String[] cmds) {
+    public PseudoClient(String hostName, int portNumber, String[] cmds, boolean commandFile) {
         this.hostname = hostName;
         this.portNumber = portNumber;
         this.cmds = cmds;
+        this.commandFile = commandFile;
         results = new ArrayList<>();
     }
     public void run() {
@@ -33,12 +39,23 @@ public class PseudoClient extends Thread {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
-            for (String cmd: cmds) {
-                LOG.info("client writing:" + cmd);
-                out.println(cmd);
-                String response = in.readLine();
-                LOG.info("adding response:" + response);
-                results.add(response);
+            if (commandFile == true) {
+                List<String> lines = Files.readAllLines(Paths.get(cmds[1]));
+                for (String cmd: lines) {
+                    LOG.info("client writing:" + cmd);
+                    out.println(cmd);
+                    String response = in.readLine();
+                    LOG.info("adding response:" + response);
+                    results.add(response);
+                }
+            } else {
+                for (String cmd: cmds) {
+                    LOG.info("client writing:" + cmd);
+                    out.println(cmd);
+                    String response = in.readLine();
+                    LOG.info("adding response:" + response);
+                    results.add(response);
+                }
             }
             clientSocket.close();
    
@@ -47,8 +64,13 @@ public class PseudoClient extends Thread {
         }
     }
     public static void main(String[] args) {
-
-        PseudoClient pc = new PseudoClient("localhost", 8089, args);
+        PseudoClient pc;
+        if (args.length == 2 && args[0].equals("file")) {
+            LOG.info("taking file!");
+            pc = new PseudoClient("localhost", 8089, args, true);
+        } else {
+            pc = new PseudoClient("localhost", 8089, args, false);
+        }
         pc.run();
     }
 }
